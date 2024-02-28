be1fc2f6886754ca6e3df22362f4f8443b236cc9
Surendernath2444 <153699161+Surendernath2444@users.noreply.github.com>
2024-02-28 12:00:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Configure Terraform to use S3 as the backend
+terraform {
+  backend "s3" {
+    bucket = "testterraformcod"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+# Define AWS provider
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIATCKARQYVEN5V2KPJ"
+  secret_key = "kYznfOkErOcQHWY8Qmlvf/KBlRGN8omGZrfCmEMW"
+}
+
+# Define AWS resource
+resource "aws_instance" "example" {
+  ami           = "ami-02ca28e7c7b8f8be1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
